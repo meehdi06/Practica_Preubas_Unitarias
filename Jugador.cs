19886dc968a590698bf38da43c55a18daaf3d3d6
meehdi06 <choaffaba@alu.edu.gva.es>
2026-03-27 19:09:47 +0100

    Add files via upload
    He subido todo lo que me ha dejado subir, he añadido mas cosas de las que tienes en la docuemtacion que te he pasado por correo.

---
+++ Jugador.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace BankAccountNS
+{
+    public class Jugador
+    {
+        private string nombre;
+        private int vida;
+        private double oro;
+        private bool npc;
+        private int resistencia;
+
+        public Jugador()
+        {
+        }
+
+        public Jugador(string nombre)
+        {
+            this.nombre = nombre;
+            this.vida = 100;
+            this.oro = 0;
+            this.npc = false;
+            this.resistencia = 50;
+        }
+
+        public string GetNombre() { return nombre; }
+        public int GetVida() { return vida; }
+        public double GetOro() { return oro; }
+        public bool GetNpc() { return npc; }
+        public int GetResistencia() { return resistencia; }
+
+        public void cambiarNombre(string nuevoNombre)
+        {
+            if (nuevoNombre == null)
+            {
+                throw new ArgumentNullException();
+            }
+            nombre = nuevoNombre;
+        }
+
+        public void anyadirOro(int cantidad)
+        {
+            oro = oro + cantidad;
+        }
+
+        public void quitarOro(int cantidad)
+        {
+            if (cantidad > oro)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            oro = oro - cantidad;
+        }
+
+        public void asignarNPC()
+        {
+            npc = true;
+        }
+
+        public void desasignarNPC()
+        {
+            npc = false;
+        }
+
+        public void anyadirResistencia()
+        {
+            if (npc == true)
+            {
+                throw new Exception("Error de resistencia, este personaje es un npc");
+            }
+            resistencia = resistencia + 10;
+        }
+
+        public void quitarResistencia()
+        {
+            if (resistencia <= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            resistencia = resistencia - 10;
+        }
+    }
+}
